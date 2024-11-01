11179a9c8bd82b5ecb957b873da8863b2c2f8c00
KurtaZhenya <zheja160@gmail.com>
2024-11-02 10:44:35 +1100

    nlp_processing.py

--- src/nlp_processing.py
+++ src/nlp_processing.py
@@ -1,1 +1,11 @@
+# src/nlp_processing.py
+import spacy
+
+class NLPProcessor:
+    def __init__(self):
+        self.nlp = spacy.load("ru_core_news_md")
+
+    def extract_entities(self, text):
+        doc = self.nlp(text)
+        return [(ent.text, ent.label_) for ent in doc.ents]
 
